346568f90caddcafa26fea5d7fea589bb5b721dd
Mikhail Klementyev <jollheef@riseup.net>
2015-10-29 11:53:49 +0300

    Initial

---
+++ main.go
@@ -0,0 +1,15 @@
+/**
+ * @file main.go
+ * @author Mikhail Klementyev jollheef<AT>riseup.net
+ * @license GNU GPLv3
+ * @date October, 2015
+ * @brief task-based ctf daemon
+ *
+ * Entry point for task-based ctf daemon
+ */
+
+package main
+
+func main() {
+
+}
