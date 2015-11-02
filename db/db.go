d65b84cc588bc0c6b06973b65f661a10f4a2ef2e
Mikhail Klementyev <jollheef@riseup.net>
2015-11-02 12:33:12 +0300

    Implements general work with database

---
+++ db/db.go
@@ -0,0 +1,109 @@
+/**
+ * @file db.go
+ * @author Mikhail Klementyev jollheef<AT>riseup.net
+ * @license GNU GPLv3
+ * @date October, 2015
+ * @brief work with database
+ *
+ * Contain functions for work with database.
+ */
+
+package db
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq" // import postgresql db engine
+)
+
+// All table names
+var tables = [...]string{}
+
+// Create tables
+func createSchema(db *sql.DB) (err error) {
+
+	_, err = db.Exec("CREATE SCHEMA IF NOT EXISTS public")
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// OpenDatabase need defer db.Close() after open
+func OpenDatabase(path string) (db *sql.DB, err error) {
+
+	db, err = sql.Open("postgres", path)
+	if err != nil {
+		return
+	}
+
+	err = createSchema(db)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// Clean all values in table
+func cleanTable(db *sql.DB, table string) (err error) {
+	_, err = db.Exec("DELETE FROM " + table)
+	return
+}
+
+// Restart id sequence in table
+func restartSequence(db *sql.DB, table string) (err error) {
+	_, err = db.Exec("ALTER SEQUENCE " + table + "_id_seq RESTART WITH 1;")
+	return
+}
+
+// CleanDatabase clean all values and restart sequences in database without
+// drop tables
+func CleanDatabase(db *sql.DB) (err error) {
+
+	for _, table := range tables {
+
+		err = cleanTable(db, table)
+		if err != nil {
+			return
+		}
+
+		err = restartSequence(db, table)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+// Drop public schema in current database
+func dropSchema(db *sql.DB) (err error) {
+
+	_, err = db.Exec("DROP SCHEMA public CASCADE")
+	if err != nil {
+		return
+	}
+	return
+}
+
+// InitDatabase recreate all database tables
+func InitDatabase(path string) (db *sql.DB, err error) {
+
+	db, err = OpenDatabase(path)
+	if err != nil {
+		return
+	}
+
+	err = dropSchema(db)
+	if err != nil {
+		// No schema not good, but ok
+	}
+
+	err = createSchema(db)
+	if err != nil {
+		return
+	}
+
+	return
+}
---
+++ db/db_test.go
@@ -0,0 +1,172 @@
+/**
+ * @file db_test.go
+ * @author Mikhail Klementyev jollheef<AT>riseup.net
+ * @license GNU GPLv3
+ * @date October, 2015
+ * @brief test general work with database functions
+ */
+
+package db
+
+import (
+	"crypto/rand"
+	"fmt"
+	"testing"
+)
+
+const dbPath string = "user=postgres dbname=henhouse_test sslmode=disable"
+
+func rndString(len int) (str string, err error) {
+
+	rand_buf := make([]byte, len)
+
+	_, err = rand.Read(rand_buf)
+	if err != nil {
+		return
+	}
+
+	str = string(rand_buf)
+
+	return
+}
+
+func TestOpenDatabase(*testing.T) {
+
+	db, err := OpenDatabase(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Error in path may not show in open
+	_, err = db.Exec("SELECT 1")
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+}
+
+func TestFailOpenDatabase(*testing.T) {
+
+	rndStr, err := rndString(15) // 15 is random len
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := OpenDatabase(fmt.Sprintf("user=%s  sslmode=disable", rndStr))
+	if err == nil {
+		panic(err)
+	}
+
+	// Error in path may not show in open
+	_, err = db.Exec("SELECT 1")
+	if err == nil {
+		panic(err)
+	}
+
+	defer db.Close()
+}
+
+func TestCreateSchema(*testing.T) {
+
+	db, err := InitDatabase(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = createSchema(db)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestInitDatabase(*testing.T) {
+
+	db, err := InitDatabase(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+}
+
+func TestCleanDatabase(*testing.T) {
+
+	db, err := InitDatabase(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = CleanDatabase(db)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestFailCleanTable(*testing.T) {
+
+	db, err := InitDatabase(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	rndStr, err := rndString(15) // 15 is random len
+	if err != nil {
+		panic(err)
+	}
+
+	// Invalid table name
+	err = cleanTable(db, rndStr)
+	if err == nil {
+		panic(err)
+	}
+}
+
+func TestFailRestartSequence(*testing.T) {
+
+	db, err := InitDatabase(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	rndStr, err := rndString(15) // 15 is random len
+	if err != nil {
+		panic(err)
+	}
+
+	// Invalid table name
+	err = restartSequence(db, rndStr)
+	if err == nil {
+		panic(err)
+	}
+}
+
+func TestFailDropSchema(*testing.T) {
+
+	db, err := InitDatabase(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	_, err = db.Exec("DROP SCHEMA public CASCADE")
+	if err != nil {
+		panic(err)
+	}
+
+	// Drop dropped schema
+	err = dropSchema(db)
+	if err == nil {
+		// must be fail
+		panic(err)
+	}
+}
