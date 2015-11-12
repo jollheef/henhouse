a8dfba1b0775d6d3aa2a6a100b6ed8fe605d5882
Mikhail Klementyev <jollheef@riseup.net>
2015-11-12 13:00:34 +0300

    Add text status to precommit script

--- precommit.sh
+++ precommit.sh
@@ -2,16 +2,27 @@
 
 TMP=`mktemp`
 
+function clean() {
+    rm ${TMP}
+}
+
+function fail() {
+    clean
+    figlet -f big FAIL
+    exit 1
+}
+
 for PKG in `go list github.com/jollheef/henhouse/... | tr '\n' ' '`; do
     figlet -f big `echo ${PKG} | sed 's|[.a-Z]*/[.a-Z]*/||'`
     echo '---------------' LINT '---------------'
     golint ${PKG} | tee ${TMP} \
-        && cat ${TMP} | wc -l | grep 0  >/dev/null && echo "ok" || exit 1
+        && cat ${TMP} | wc -l | grep 0  >/dev/null && echo "ok" || fail
     echo
     echo '---------------' TEST '---------------'
-    go test -v -covermode=count -coverprofile=coverage.out ${PKG} || exit 1
+    go test -v -covermode=count -coverprofile=coverage.out ${PKG} || fail
     echo
     echo
 done
 
-rm ${TMP}
+clean
+figlet -f big OK
