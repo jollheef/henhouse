45368bf3c83d69a5ea678a01f1004cb9ee6af1f3
Mikhail Klementyev <jollheef@riseup.net>
2015-11-12 12:50:40 +0300

    Now fail precommit script if lint check failed

--- precommit.sh
+++ precommit.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+TMP=`mktemp`
+
 for PKG in `go list github.com/jollheef/henhouse/... | tr '\n' ' '`; do
     figlet -f big `echo ${PKG} | sed 's|[.a-Z]*/[.a-Z]*/||'`
     echo '---------------' LINT '---------------'
-    golint ${PKG}
+    golint ${PKG} | tee ${TMP} \
+        && cat ${TMP} | wc -l | grep 0  >/dev/null && echo "ok" || exit 1
     echo
     echo '---------------' TEST '---------------'
     go test -v -covermode=count -coverprofile=coverage.out ${PKG} || exit 1
     echo
     echo
 done
+
+rm ${TMP}
