2c324e8103e4c8950b48f50f31afcfca768b7aec
Mikhail Klementyev <jollheef@riseup.net>
2015-11-12 10:41:15 +0300

    Implements category table [2]

---
+++ db/category.go
@@ -0,0 +1,70 @@
+/**
+ * @file category.go
+ * @author Mikhail Klementyev jollheef<AT>riseup.net
+ * @license GNU GPLv3
+ * @date November, 2015
+ * @brief queries for category table
+ */
+
+package db
+
+import "database/sql"
+
+type Category struct {
+	Id   int
+	Name string
+}
+
+func createCategoryTable(db *sql.DB) (err error) {
+
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS "category" (
+		id	SERIAL PRIMARY KEY,
+		name	TEXT NOT NULL
+	)`)
+
+	return
+}
+
+// AddCategory add category to db and fill Id
+func AddCategory(db *sql.DB, category *Category) (err error) {
+
+	stmt, err := db.Prepare("INSERT INTO category (name) " +
+		"VALUES ($1) RETURNING id")
+	if err != nil {
+		return
+	}
+
+	defer stmt.Close()
+
+	err = stmt.QueryRow(category.Name).Scan(&category.Id)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// GetCategories get all categories in category table
+func GetCategories(db *sql.DB) (categories []Category, err error) {
+
+	rows, err := db.Query("SELECT id, name FROM category")
+	if err != nil {
+		return
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var category Category
+
+		err = rows.Scan(&category.Id, &category.Name)
+		if err != nil {
+			return
+		}
+
+		categories = append(categories, category)
+	}
+
+	return
+}
---
+++ db/category_test.go
@@ -0,0 +1,83 @@
+/**
+ * @file category_test.go
+ * @author Mikhail Klementyev jollheef<AT>riseup.net
+ * @license GNU GPLv3
+ * @date November, 2015
+ * @brief test work with category table
+ */
+
+package db
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestCreateCategoryTable(*testing.T) {
+
+	db, err := InitDatabase(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = createCategoryTable(db)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestAddCategory(*testing.T) {
+
+	db, err := InitDatabase(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	category := Category{Id: 255, Name: "test"}
+
+	err = AddCategory(db, &category)
+	if err != nil {
+		panic(err)
+	}
+
+	if category.Id != 1 {
+		panic(errors.New("Category id not correct"))
+	}
+}
+
+func TestGetCategories(*testing.T) {
+
+	db, err := InitDatabase(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	categories := 150
+
+	for i := 0; i < categories; i++ {
+
+		err = AddCategory(db, &Category{Name: fmt.Sprintf("%d", i)})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	cats, err := GetCategories(db)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(cats) != categories {
+		panic(errors.New("Mismatch get categories length"))
+	}
+
+	for i := 0; i < categories; i++ {
+
+		if cats[i].Name != fmt.Sprintf("%d", i) && cats[i].Id != i {
+			panic(errors.New("Get invalid category"))
+		}
+	}
+}
