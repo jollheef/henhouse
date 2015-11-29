366d99ab10a94a11fb7238fdc2b220e0d738b1dd
Mikhail Klementyev <jollheef@riseup.net>
2015-11-29 19:18:19 +0300

    Implements basic web scoreboard

---
+++ scoreboard/scoreboard.go
@@ -0,0 +1,167 @@
+/**
+ * @file scoreboard.go
+ * @author Mikhail Klementyev jollheef<AT>riseup.net
+ * @license GNU GPLv3
+ * @date November, 2015
+ * @brief web scoreboard
+ *
+ * Contain web ui and several helpers for convert results to table
+ */
+
+package scoreboard
+
+import (
+	"fmt"
+	"github.com/jollheef/henhouse/game"
+	"golang.org/x/net/websocket"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	contestStateNotAvailable = "state n/a"
+	contestNotStarted        = "not started"
+	contestRunning           = "running"
+	contestCompleted         = "completed"
+)
+
+var (
+	startTime     time.Time
+	endTime       time.Time
+	currentResult string
+	contestStatus string
+	lastUpdated   string
+)
+
+func getInfo() string {
+
+	var left time.Duration
+	var btnType string
+
+	now := time.Now()
+
+	if now.Before(startTime) {
+
+		contestStatus = contestNotStarted
+		left = startTime.Sub(now)
+		btnType = "warning"
+
+	} else if now.Before(endTime) {
+
+		contestStatus = contestRunning
+		left = endTime.Sub(now)
+		btnType = "success"
+
+	} else {
+		contestStatus = contestCompleted
+		left = time.Duration(0)
+		btnType = "primary"
+	}
+
+	info := fmt.Sprintf(`<span class="btn btn-%s">contest %s</span>`+
+		`<span class="btn btn-info">Left %s</span>`+
+		`<span class="btn btn-info">Updated at %s</span>`,
+		btnType, contestStatus, left, lastUpdated)
+
+	return info
+}
+
+func infoHandler(ws *websocket.Conn) {
+
+	defer ws.Close()
+	for {
+		_, err := fmt.Fprint(ws, getInfo())
+		if err != nil {
+			log.Println("Socket closed:", err)
+			return
+		}
+
+		time.Sleep(time.Second)
+	}
+}
+
+func scoreboardHandler(ws *websocket.Conn) {
+
+	defer ws.Close()
+
+	fmt.Fprint(ws, currentResult)
+	sendedResult := currentResult
+	lastUpdate := time.Now()
+
+	for {
+		if sendedResult != currentResult ||
+			time.Now().After(lastUpdate.Add(time.Minute)) {
+
+			sendedResult = currentResult
+			lastUpdate = time.Now()
+
+			_, err := fmt.Fprint(ws, currentResult)
+			if err != nil {
+				log.Println("Socket closed:", err)
+				return
+			}
+		}
+
+		time.Sleep(time.Second)
+	}
+}
+
+func resultUpdater(game *game.Game, updateTimeout time.Duration) {
+
+	head := "<thead><th>#</th><th>Team</th><th>Score</th></thead>"
+
+	for {
+		result := head
+
+		result += "<tbody>"
+
+		scores, err := game.Scoreboard()
+		if err != nil {
+			log.Println("Get scoreboard fail:", err)
+			time.Sleep(updateTimeout)
+			continue
+		}
+
+		for n, teamScore := range scores {
+			result += fmt.Sprintf(
+				"<tr><td>%d</td><td>%s</td><td>%d</td><tr>",
+				n, teamScore.Name, teamScore.Score)
+
+		}
+
+		result += "</tbody>"
+
+		currentResult = result
+
+		now := time.Now()
+		lastUpdated = fmt.Sprintf("%02d:%02d:%02d", now.Hour(),
+			now.Minute(), now.Second())
+
+		time.Sleep(updateTimeout)
+	}
+}
+
+// Scoreboard implements web scoreboard
+func Scoreboard(game *game.Game, wwwPath, addr string,
+	start, end time.Time) (err error) {
+
+	contestStatus = contestStateNotAvailable
+	startTime = start
+	endTime = end
+
+	go resultUpdater(game, time.Second)
+
+	http.Handle("/info", websocket.Handler(infoHandler))
+	http.Handle("/scoreboard", websocket.Handler(scoreboardHandler))
+	http.Handle("/", http.FileServer(http.Dir(wwwPath)))
+
+	log.Println("Launching scoreboard at", addr)
+
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		return
+	}
+
+	return
+}
