fd23696a4e406c86439ab5377bc39c1c8a5fe5dc
Mikhail Klementyev <jollheef@riseup.net>
2016-11-15 16:43:12 +0300

    Add english translation field to database

--- db/task.go
+++ db/task.go
@@ -18,6 +18,8 @@ type Task struct {
 	ID            int
 	Name          string
 	Desc          string
+	NameEn        string
+	DescEn        string
 	Tags          string
 	CategoryID    int
 	Level         int
@@ -38,6 +40,8 @@ func createTaskTable(db *sql.DB) (err error) {
 		id		SERIAL PRIMARY KEY,
 		name		TEXT NOT NULL,
 		description	TEXT NOT NULL,
+		name_en		TEXT NOT NULL,
+		description_en	TEXT NOT NULL,
 		tags		TEXT NOT NULL,
 		category_id	INTEGER NOT NULL,
 		level		INTEGER NOT NULL,
@@ -57,19 +61,21 @@ func createTaskTable(db *sql.DB) (err error) {
 // AddTask add task and fill id
 func AddTask(db *sql.DB, t *Task) (err error) {
 
-	stmt, err := db.Prepare("INSERT INTO task (name, description, tags, " +
+	stmt, err := db.Prepare("INSERT INTO task (name, description, " +
+		"name_en, description_en, tags, " +
 		"category_id, level, price, shared, flag, max_share_price, " +
 		"min_share_price, opened, author, opened_time) " +
-		"VALUES ($1, $2, $3, $4, " +
-		"$5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING id")
+		"VALUES ($1, $2, $3, $4, $5, " +
+		"$6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING id")
 	if err != nil {
 		return
 	}
 
 	defer stmt.Close()
 
-	err = stmt.QueryRow(t.Name, t.Desc, t.Tags, t.CategoryID, t.Level,
-		t.Price, t.Shared, t.Flag, t.MaxSharePrice, t.MinSharePrice,
+	err = stmt.QueryRow(t.Name, t.Desc, t.NameEn, t.DescEn, t.Tags,
+		t.CategoryID, t.Level, t.Price, t.Shared, t.Flag,
+		t.MaxSharePrice, t.MinSharePrice,
 		t.Opened, t.Author, t.OpenedTime).Scan(&t.ID)
 	if err != nil {
 		return
@@ -81,7 +87,8 @@ func AddTask(db *sql.DB, t *Task) (err error) {
 // GetTasks get all tasks in tasks table
 func GetTasks(db *sql.DB) (tasks []Task, err error) {
 
-	rows, err := db.Query("SELECT id, name, description, tags, category_id, " +
+	rows, err := db.Query("SELECT id, name, description, name_en, " +
+		"description_en, tags, category_id, " +
 		"level, price, shared, flag, max_share_price, " +
 		"min_share_price, opened, author, opened_time FROM task")
 	if err != nil {
@@ -93,7 +100,8 @@ func GetTasks(db *sql.DB) (tasks []Task, err error) {
 	for rows.Next() {
 		var t Task
 
-		err = rows.Scan(&t.ID, &t.Name, &t.Desc, &t.Tags, &t.CategoryID,
+		err = rows.Scan(&t.ID, &t.Name, &t.Desc, &t.NameEn, &t.DescEn,
+			&t.Tags, &t.CategoryID,
 			&t.Level, &t.Price, &t.Shared, &t.Flag,
 			&t.MaxSharePrice, &t.MinSharePrice, &t.Opened,
 			&t.Author, &t.OpenedTime)
@@ -130,16 +138,18 @@ func SetOpened(db *sql.DB, taskID int, opened bool) (err error) {
 func UpdateTask(db *sql.DB, t *Task) (err error) {
 
 	stmt, err := db.Prepare("UPDATE task SET name=$1, description=$2, " +
-		"tags=$3, category_id=$4, level=$5, price=$6, shared=$7, flag=$8, " +
-		"max_share_price=$9, min_share_price=$10, opened=$11, " +
-		"author=$12, opened_time=$13 WHERE id=$14")
+		"name_en=$3, description_en=$4, " +
+		"tags=$5, category_id=$6, level=$7, price=$8, shared=$9, flag=$10, " +
+		"max_share_price=$11, min_share_price=$12, opened=$13, " +
+		"author=$14, opened_time=$15 WHERE id=$16")
 	if err != nil {
 		return
 	}
 
 	defer stmt.Close()
 
-	_, err = stmt.Exec(t.Name, t.Desc, t.Tags, t.CategoryID, t.Level, t.Price,
+	_, err = stmt.Exec(t.Name, t.Desc, t.NameEn, t.DescEn, t.Tags,
+		t.CategoryID, t.Level, t.Price,
 		t.Shared, t.Flag, t.MaxSharePrice, t.MinSharePrice, t.Opened,
 		t.Author, t.OpenedTime, t.ID)
 	if err != nil {
@@ -152,7 +162,8 @@ func UpdateTask(db *sql.DB, t *Task) (err error) {
 // GetTask get task by id
 func GetTask(db *sql.DB, taskID int) (t Task, err error) {
 
-	stmt, err := db.Prepare("SELECT id, name, description, tags, category_id, " +
+	stmt, err := db.Prepare("SELECT id, name, description, name_en, " +
+		"description_en, tags, category_id, " +
 		"level, price, shared, flag, max_share_price, " +
 		"min_share_price, opened, author, opened_time " +
 		"FROM task WHERE id=$1")
@@ -162,7 +173,8 @@ func GetTask(db *sql.DB, taskID int) (t Task, err error) {
 
 	defer stmt.Close()
 
-	err = stmt.QueryRow(taskID).Scan(&t.ID, &t.Name, &t.Desc, &t.Tags,
+	err = stmt.QueryRow(taskID).Scan(&t.ID, &t.Name, &t.Desc,
+		&t.NameEn, &t.DescEn, &t.Tags,
 		&t.CategoryID, &t.Level, &t.Price, &t.Shared, &t.Flag,
 		&t.MaxSharePrice, &t.MinSharePrice, &t.Opened,
 		&t.Author, &t.OpenedTime)
