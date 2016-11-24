bb71bef62799972d05555fcaa252a324fc3593db
Mikhail Klementyev <jollheef@riseup.net>
2016-11-24 14:16:29 +0300

    Automatic select websocket protocol (ws/wss)

--- scoreboard/www/js/scoreboard.js
+++ scoreboard/www/js/scoreboard.js
@@ -1,10 +1,15 @@
-var scoreboard = new WebSocket("ws://" + location.host + "/scoreboard");
+if (location.protocol == 'https:')
+    var protocol = "wss://";
+else
+    var protocol = "ws://";
+
+var scoreboard = new WebSocket(protocol + location.host + "/scoreboard");
 
 scoreboard.onmessage = function(e) {
     document.getElementById('scoreboard-table').innerHTML = e.data
 }
 
-var info = new WebSocket("ws://" + location.host + "/info");
+var info = new WebSocket(protocol + location.host + "/info");
 
 info.onmessage = function(e) {
     document.getElementById('info').innerHTML = e.data
--- scoreboard/www/js/tasks.js
+++ scoreboard/www/js/tasks.js
@@ -1,10 +1,15 @@
-var scoreboard = new WebSocket("ws://" + location.host + "/tasks");
+if (location.protocol == 'https:')
+    var protocol = "wss://";
+else
+    var protocol = "ws://";
+
+var scoreboard = new WebSocket(protocol + location.host + "/tasks");
 
 scoreboard.onmessage = function(e) {
     document.getElementById('tasks-table').innerHTML = e.data
 }
 
-var info = new WebSocket("ws://" + location.host + "/info");
+var info = new WebSocket(protocol + location.host + "/info");
 
 info.onmessage = function(e) {
     document.getElementById('info').innerHTML = e.data
